0bc49b0dc4b7ce8e4609b605e8331314f90d4c41
tielens <elske.tielens@gmail.com>
2020-01-08 11:41:29 -0600

    alternative lunch suggestion

---
+++ warm_bowl.py
@@ -0,0 +1,11 @@
+Proposing an alternative to a sandwich:
+
+warm bowl with grains and protein
+
+ingredients:
+chickpeas
+mango chutney
+lettuce
+couscous
+fresh herbs
+cheese- what kind of cheese do you like?
